0d28ba3f4cd246f904be7a78a614ab507abf39ec
madhumitaaa <madhumita.mandal.ee26@heritageit.edu.in>
2026-03-10 00:08:46 +0530

    modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { PrivateRoute, PublicRoute } from './routes/PrivateRoute';
 import AppLayout from './components/layout/AppLayout';
